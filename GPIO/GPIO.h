6c8f599b1d43a89a10adebfceeacbad46d677bcc
Ahmed Wael Salah <88168055+xSIayer@users.noreply.github.com>
2023-05-13 00:28:57 +0300

    Update GPIO.h

--- GPIO/GPIO.h
+++ GPIO/GPIO.h
@@ -1,6 +1,7 @@
 #ifndef GPIO_H
 #define GPIO_H
-#include "bit_utilies.h" //Bit-functions
+#include "bit_util.h" //Bit-functions
+#include "TM4C123.h"
 #define RED 0x02
 #define GREEN 0x08
 #define YELLOW 0x0A
