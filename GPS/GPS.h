c588c8362b1815d9236c7e5e976c599839225df5
Ahmed Wael Salah <88168055+xSIayer@users.noreply.github.com>
2023-05-13 00:28:10 +0300

    Delete GPS directory

--- GPS/GPS.h
+++
@@ -1,8 +0,0 @@
-#ifndef GPS
-#define GPS
-float toRad (float angle);
-float toDeg (float angle);
-float getDistance (float startLong, float startLat,float endLong,float endLat);
-void GPS_read ();
-void GPS_format ();
-#endif
