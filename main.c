d5af0ea5ac4bbcbb6b11e4c966cbf5a47458f3b7
Omar-Ayman22 <132768576+Omar-Ayman22@users.noreply.github.com>
2023-05-14 18:49:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,104 +4,106 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
-const double EARTHRADIUS = 6371000;
-const double PI = 3.14159265358979323846;
 
-char GPS_string[80] = "";
-char GPS_logName[] = "$GPRMC,";
-char GPS_formated[12][20];
-char *token;
-float currentLat, currentLong, speed;
-float endLong = 30 ;
-float endLat=31;
-float displacement;
+#define EARTHRADIUS 6371000.0 // Earth's radius in meters
+const double PI=3.14159265358979323846;
 
-float  toDeg(float angle) {
-    int degree = angle / 100;
-    int minutes = angle - degree * 100;
-    return (degree + minutes / 60);
+
+ void GPSscan(char * lat, char * longt) //Get the message from gps then extract latitude and longitude
+	 {
+char d;
+int i=0,j=0,result=1;
+
+do
+{
+	d=UART5_GPS_read();  //d="$GPRMC"
+}while(d!=',');
+
+
+ do
+{
+ d = UART5_GPS_read(); //numbers that represent time
+} while (d != ',');
+ 
+d = UART5_GPS_read(); //'A'
+if(d=='A'){
+ d = UART5_GPS_read(); //','
+ d = UART5_GPS_read(); // 'First latitude number (3)'
+while (d != ',') // loop to take the latitude
+	{
+    lat[i++] = d; //latitude array
+  d = UART5_GPS_read();
+ }
+   lat[i] = '\0';
+
+   d = UART5_GPS_read(); //'N'
+		if(d=='N'){
+			d = UART5_GPS_read();  //','
+			d = UART5_GPS_read(); // 'First longitude number (0)'
+ while (d != ',') {
+longt[j++] = d; // loop to take the longitude
+  d = UART5_GPS_read();
+  }
+ longt[j] = '\0';
+result = 0; 
+}
+}
 }
 
-float  toRad(float angle) {
+
+double toRad(double angle) {
     return (angle * PI / 180);
 }
-//this function returns the distance between our current coordinations and the final coordination
-float  getDistance(int startLong, int startLat, int endLong, int endLat) {
-    float lamda_A = toRad(toDeg(startLong));
-    float phi_A = toRad(toDeg(startLat));
-    float lamda_B = toRad(toDeg(endLong));
-    float phi_B = toRad(toDeg(endLat));
-    float a = pow(sin((phi_B - phi_A) / 2), 2) + cos(phi_A) * cos(phi_B) * pow(sin((lamda_B - lamda_A) / 2), 2);
+
+double getDistance(double startLong, double startLat, double endLong, double endLat) {
+    double lambda_A = toRad(startLong);
+    double phi_A = toRad(startLat);
+    double lambda_B = toRad(endLong);
+    double phi_B = toRad(endLat);
+    double delta_lambda = lambda_B - lambda_A;
+    double delta_phi = phi_B - phi_A;
+    double a = pow(sin(delta_phi / 2), 2) + cos(phi_A) * cos(phi_B) * pow(sin(delta_lambda / 2), 2);
     double c = 2 * atan2(sqrt(a), sqrt(1 - a));
     return EARTHRADIUS * c;
 }
 
-int GPS_read() {
-    char receivedChar;
-    char flag;
-    char i;
-    //do {
-        flag = 1;
-        for (i = 0; i < 7; i++) {
-            if (UART5_GPS_read() != GPS_logName[i]) {
-                flag = 0;
-								return 0;							// removing flag assignment
-            }
-        }
-  //  } while (flag == 0); // checking for desired log name
-    do {
-        static char GPS_counter = 0; // using static to retain value between function calls
-        receivedChar = UART5_GPS_read();
-        GPS_string[GPS_counter++] = receivedChar;
-    } while (receivedChar != '*');
-		return 1;
+
+float toDECIMALDEGREE(float coordinate) //change the degree minutes received from gps to decimal degree
+	{
+    int degrees = (int)(coordinate / 100);
+    float minutes = coordinate - degrees * 100;
+    float decimalDegrees = degrees + minutes / 60.0;
+
+    return decimalDegrees;
 }
 
-void GPS_format() {
-    char noOfTokenStrings = 0;
-    token = strtok(GPS_string, ",");
-    do {
-        strcpy(GPS_formated[noOfTokenStrings], token);
-        token = strtok(NULL, ",");
-        noOfTokenStrings++;
-    } while (token != NULL);
-    if (strcmp(GPS_formated[1], "A") == 0) { // Valid
-        if (strcmp(GPS_formated[3], "N") == 0)
-            currentLat = atoi(GPS_formated[2]);
-        else
-            currentLat = -atoi(GPS_formated[2]);
-        if (strcmp(GPS_formated[5], "E") == 0)
-            currentLong = atoi(GPS_formated[4]);
-        else
-            currentLong = -atoi(GPS_formated[4]);
-    }
-}
 
 int main(void){
-	int p;
-	char a;
-  initPortF();
+
+	char i,latitude[20],longitude[20];
+	double displacement,lat1,lon1,lat2,lon2;
+	double lonFINAL= 31.627247;//the end destination long..
+	double latFINAL=30.149965;// the end destination lat...
+        initPortF();
 	initPortD();
 	UART5_GPS_init();
 	UART0_GPS_init();
+	
+	
 	while(1){
-		p=GPS_read();
-		if(p==0)
-		{
-			UART0_GPS_write('e');
-			waitseconds(1);
-		}
-		else
-		{
-		GPS_format();
-		displacement=getDistance(currentLong,currentLat,endLong,endLat);
-		if(displacement>5)
+
+		GPSscan(latitude,longitude);
+		lat1 =atof(latitude);
+		lon1 = atof(longitude);
+		lat2 = toDECIMALDEGREE(lat1);
+		lon2 = toDECIMALDEGREE(lon1);
+		displacement=getDistance(lon2,lat2,lonFINAL,latFINAL);
+		if(displacement>20)
 			turn_led(RED);
-		else if(displacement<5)
+		else if(displacement<20 &&displacement>10)
 			turn_led(YELLOW);
 		else 
 			turn_led(GREEN);
-		}
-	
+	  	
 }
 }
